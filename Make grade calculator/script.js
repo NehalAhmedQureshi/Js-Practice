b41a72c340252a7461d43aa875a21105a5fbe0e6
Nehal Ahmed <m.nehalq125@gmail.com>
2024-03-14 00:35:45 +0500

    make grade calculator using js by if-else statement

---
+++ Make grade calculator/script.js
@@ -0,0 +1,38 @@
+let grade_calculator = prompt("Enter your Percentage % ");
+
+if (grade_calculator > 100) {
+
+    document.write("You write invalid percentage sorry! 😥");
+
+} else if (grade_calculator >= 90) {
+    document.write("Congratulations! 🎉 <br> Your grade is A<sup>+1</sup> ");
+}
+else if (grade_calculator >= 80) {
+    document.write("Congratulations! 🎉 <br> Your grade is A<sup>+</sup> ");
+}
+else if (grade_calculator >= 70) {
+    document.write("Congratulations! 🎉 <br> Your grade is A ");
+}
+else if (grade_calculator >= 60) {
+    document.write("Betterluck! 🎉 <br> Your grade is B ");
+}
+else if (grade_calculator >= 50) {
+    document.write("Betterluck! 🎉 <br> Your grade is C ");
+}
+else if (grade_calculator >= 40) {
+    document.write("Need Hard Work! 😕 <br> Your grade is D ");
+}
+else if (grade_calculator >= 30) {
+    document.write("Need Hard Work! 😕 <br> Your grade is E ");
+}
+else if (grade_calculator >= 30) {
+    document.write("Need Hard Work! 😕 <br> Your grade is E ");
+}
+else if (grade_calculator >= 0) {
+    document.write("Opps! ☹ <br> You are Failed! ");
+}
+else {
+    document . write ("Please put invalid percentage 😞");
+}
+
+
