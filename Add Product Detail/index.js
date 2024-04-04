187c8935f0f23fb9c5e3dc6d9ebf56a8098e8312
Nehal Ahmed Qureshi <m.nehalq125@gmail.com>
2024-04-05 03:21:16 +0500

    complete

--- Add Product Detail/index.js
+++ Add Product Detail/index.js
@@ -1,5 +1,22 @@
-let productName = prompt("1- Enter Product Name ?");
-let productPrice = prompt("2- Enter Product Prize ?");
-let productDetail = prompt("3- Enter Product Details");
+let form = document.querySelector("#productInfo");
+let name = document.querySelector("#productName");
+let detail = document.querySelector("#productDetail");
+let price = document.querySelector("#productPrice");
+let print_1 = document.querySelector("#name");
+let print_2 = document.querySelector("#detail");
+let print_3 = document.querySelector("#price")
 
-document.write(productDetail)
+
+form.addEventListener("submit",(event ) =>{
+    event.preventDefault()
+
+    const productName = name.value
+    const productDetail = detail.value
+    const productPrice = price.value
+
+    print_1.innerHTML = `Product Name: ${productName} `;
+    print_2.innerHTML = `Product Detail: ${productDetail}`;
+    print_3.innerHTML = `Product Price; ${productPrice}`;
+
+
+})
--- Table Calculator/script.js
+++ Table Calculator/script.js
@@ -1,6 +1,48 @@
-let tableOf = 2;
+// let inputForm = document.querySelector("#table");
+// let input = document.querySelector("#inputID");
+// let table = document.querySelector(".time-table");
 
-for(let i = 1 ; i <= 12 ; i++){
-    console.log(tableOf + " x " + i + " = " , tableOf * i);
-} 
+// // https://www.w3schools.com/js/js_htmldom_eventlistener.asp
+// // https://developer.mozilla.org/en-US/docs/Web/Events
 
+// inputForm.addEventListener("submit", (event) => {
+//   event.preventDefault();
+
+//   let tableOf = input.value;
+
+//   table.innerHTML = "";
+
+//   for (let i = 1; i <= 12; i++) {
+//     //   console.log(tableOf + " x " + i + " =", tableOf * i);
+//     const message = `${tableOf} X ${i} = ${tableOf * i}`;
+
+//     table.innerHTML += `${message} <br>`;
+//   }
+// });
+
+// // navigator.clipboard.writeText("Chandio");
+// // navigator.clipboard
+// //   .readText()
+// //   .then(
+// //     (clipText) => (document.querySelector(".editor").innerText += clipText)
+// //   );
+
+let tableForm = document.querySelector("#tableForm")
+let input = document.querySelector("#inputID")
+let printTable = document.querySelector("#time-table")
+
+tableForm.addEventListener("submit" , (event) => {
+    event.preventDefault();
+
+    let tableOf = input.value;
+    printTable.innerHTML = "" ;
+
+    for (let i = 0; i <= 12; i++) {
+        
+        const message = `${tableOf} x ${i} = ${tableOf * i}`
+
+        printTable.innerHTML = `${message} <br>`
+    }
+
+}
+);
